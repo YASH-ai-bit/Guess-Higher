8d2d73bd98371f1834154bdbf26dc18fe31bfc21
Yash-ai-bit <gujjaryash7089@gmail.com>
2025-03-25 01:55:22 +0000

    random commit 4

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import wordRoutes from "./routes/Word.route.js";
 import userRoutes from "./routes/User.route.js";
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.BASE_URL;
 
 const app = express();
 
@@ -22,7 +22,7 @@ app.use("/api/users", userRoutes);
 
 //connect db
 mongoose.connect(process.env.MONGO_URI).then(() => {
-  app.listen([PORT, "https://guess-higher-l868.vercel.app"], () => {
+  app.listen(BASE_URL, () => {
     console.log("Connected to db andListening on port 8080");
   });
 });
